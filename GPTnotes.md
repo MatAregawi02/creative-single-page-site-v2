9489533bfbbf42e561e767fef84edcee43e8d462
MatAregawi02 <mataregwi@gmail.com>
2025-02-02 12:29:58 -0500

    Fixed navigation menu issues for all screen sizes

--- GPTnotes.md
+++ GPTnotes.md
@@ -27,6 +27,28 @@
 
 # The use of srcset attribute to reduce repetitive html...
 
-<!-- <img src="image-default.jpg" 
+<img src="image-default.jpg" 
      srcset="image-small.jpg 500w, image-medium.jpg 1000w, image-large.jpg 2000w" 
-     alt="example image"> --!>
+     alt="example image">
+
+# Unexpected behavior of navigation menu upon resizing of window
+
+The code I wrote for toggling the menu worked just fine, but I noticed a problem when I tried to make the webpage responsive. Whenever I resize the window to 600px or less(mobile width), the menu would appear without my clicking the hamburger icon. To provide a more robust solution for the problem, I asked chatGPT for help.
+
+ChatGPT suggested that I separate the clicking functionality from the displaying functionality of the navigation:
+
+function updateNavVisibility(){
+    const screenWidth = document.documentElement.clientWidth; //calculate current screen width
+    if (screenWidth <= 600) { //if mobile width
+        navigation.classList.add("hidden"); //hide the menu
+        hamburgerIcon.style.display = "inline-block"; //hamburger menu appears
+        crossIcon.style.display = "none"; //cross icon is toggled off
+    } 
+    else {
+        navigation.classList.remove("hidden"); //if other width, regular menu shown
+    }
+}
+
+window.addEventListener('resize',updateNavVisibility); //up on resize, make necessary changes to the menu
+updateNavVisibility(); //initial setup on page load
+//when the page is loaded, the correct menu form is shown based on the screen size!
--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 let hamburgerIcon = document.querySelector(".hamburger-icon");
 let crossIcon = document.querySelector(".cross-icon");
 let navigation = document.querySelector("nav");
-let menu = document.querySelector("ul");
 let leftArrow = document.querySelector(".left-arrow");
 let rightArrow = document.querySelector(".right-arrow");
 let cardHeading = document.querySelector("h5");
@@ -13,20 +12,30 @@ let captionDate = document.querySelector(".caption-date");
 
 let tabletWidth = 768; //`${768}px` is not a number!
 let mobileWidth = 600;
-// let mobileHeroImage = document.querySelector(".mobile-hero-img");
-// let tabletHeroImage = document.querySelector(".tablet-hero-img");
-// let desktopHeroImage = document.querySelector(".desktop-hero-img");
 
 
+//function adopted from chatGPT for learning purposes (modified)
+//please read GPTnotes.md for documentation 
+function updateNavVisibility(){ //this function is for correct displaying of the nav menu
+    const screenWidth = document.documentElement.clientWidth;
+    if (screenWidth <= 600) {
+        navigation.classList.add("hidden"); 
+        hamburgerIcon.style.display = "inline-block";
+        crossIcon.style.display = "none"; 
+    } 
+    else {
+        navigation.classList.remove("hidden"); 
+        // hamburgerIcon.style.display = "none"; //solved in sass
+        // crossIcon.style.display = "none"; 
+    }
+}
 
 hamburgerIcon.addEventListener("click", () => {
     
         hamburgerIcon.style.display = "none"; //upon a click, it should disappear
         crossIcon.style.display = "inline-block";
         navigation.classList.remove("hidden");
-        
-   
-})
+});
 crossIcon.addEventListener("click", () => {
         crossIcon.style.display = "none";
         hamburgerIcon.style.display = "inline-block";
@@ -34,18 +43,8 @@ crossIcon.addEventListener("click", () => {
         
 });
 
-window.addEventListener('resize',function(){
-    const screenWidth = this.window.innerWidth;
-    
-    if (screenWidth > 600){
-        navigation.classList.remove("hidden");
-    }
-    if (screenWidth === 600){
-        navigation.classList.add("hidden");
-        hamburgerIcon.style.display = "inline-block";
-        crossIcon.style.display = "none";
-    }
-});
+window.addEventListener('resize',updateNavVisibility); //returns it to starting point
+updateNavVisibility(); //updates the navigation upon each resize...
 
 //section three, dynamic content switching??
 //an array of images
@@ -75,22 +74,25 @@ const slides = [
 let currentSlide = 0; //starting point
 
 function updateSlide(slideData){
-    let screenWidth = window.innerWidth;
+    let screenWidth = document.documentElement.clientWidth;
+
     cardHeading.textContent = slideData.title;
     captionTitle.textContent = slideData.captionHead;
     captionDate.textContent = slideData.captionDate;
    
-    if (screenWidth > tabletWidth){
+  
         slideImage.src = slideData.image;
-    }
     
-    if (screenWidth <= tabletWidth){
+  
+    if (screenWidth <= mobileWidth){ //mobile should be checked first!!
+            slideImageMobile.src = slideData.imageMobile;
+   
+    }
+    else if (screenWidth <= tabletWidth){
         slideImageTablet.src = slideData.imageTablet;
+        
     }
 
-    if (screenWidth <= mobileWidth){
-        slideImageMobile.src = slideData.imageMobile;
-    }
  
 }
 
@@ -119,3 +121,11 @@ leftArrow.addEventListener("click", () => {
         leftArrow.style.display = "none";
     }
 });
+
+//this snippet below makes sure that the slides get updated to current slide 
+//up on resizing of the window....
+function updateSlideUponResize(){
+    const slideData = slides[currentSlide];
+    updateSlide(slideData);
+}
+window.addEventListener('resize',updateSlideUponResize);
